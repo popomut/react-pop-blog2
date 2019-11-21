b38dc64cc935a6782e63cad24afd4fdd4d34de4f
popomut <whitebaldur@gmail.com>
2019-11-21 11:54:39 +0700

    2 files modified, 1 added

--- ShowArticle.js
+++ ShowArticle.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import "react-mde/lib/styles/css/react-mde-all.css";
 import firebase from "./firebase/Firebase";
+import MarkdownRenderer from "./MarkdownRenderer";
 
 
 
@@ -25,10 +26,87 @@ class ShowArticle extends Component {
 
   }
 
+getData(e) {
 
+  var id = this.props.match.params.id;
+
+  firebase
+      .database()
+      .ref("myblog/" + id)
+      .once("value")
+      .then(snapshot => {
+        const key = snapshot.key;
+        const val = snapshot.val();
+        //alert(val.value);
+        console.log(val.value);
+
+        this.setState({
+          value: val.value
+        });
+      })
+      .catch(e => {
+        console.log("Error fetching data", e);
+      });
+
+/*
+    firebase
+      .database()
+      .ref("myblog")
+      .once("value")
+      .then(snapshot => {
+        const key = snapshot.key;
+        const val = snapshot.val();
+        //console.log(val);
+
+        this.setState({
+          value: val
+        });
+      })
+      .catch(e => {
+        console.log("Error fetching data", e);
+      });
+
+      */
+
+
+/*
+const ref = firebase.firestore().collection('myblog').doc(id);
+    ref.get().then((doc) => {
+      if (doc.exists) {
+
+        console.log(doc.data);
+
+        this.setState({
+          
+          value: doc.data()
+
+        });
+
+      } else {
+        console.log("No such document!");
+      }
+    });
+
+    */
+
+
+/*
+  var ref = firebase.database().ref("myblog");
+var query = ref.doc(id).get();
+query.once("value", function(snapshot) {
+  snapshot.forEach(function(child) {
+    console.log(child.key, child.val().bio);
+  });
+});
+*/
+
+//db.collection('books').doc('fK3ddutEpD2qQqRMXNW5').get()
+
+  }
 
   componentDidMount() {
-    //this.getData();
+    //var id = this.props.match.params.id;
+    this.getData();
   }
 
 
@@ -41,6 +119,7 @@ class ShowArticle extends Component {
         <br />
         <br />
         Show Article
+        <MarkdownRenderer data={this.state.value}/>
       </div>
     );
   }
